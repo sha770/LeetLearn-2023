57beaf79efb5151b366badfe9baef68c711af4b0
sha770 <99661850+sha770@users.noreply.github.com>
2023-10-28 15:52:31 +0530

    Time: 13 ms (5.83%), Space: 20.7 MB (39.38%) - LeetHub

---
+++ 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int i=0,j=nums.size()-1;
+        int ans=0;
+        while(i<j){
+            if(nums[i]+nums[j]<target){
+                ans+=j-i;
+                i++;
+            }
+            else if(nums[i]+nums[j]>=target) j--;
+        }
+        return ans;
+    }
+};
