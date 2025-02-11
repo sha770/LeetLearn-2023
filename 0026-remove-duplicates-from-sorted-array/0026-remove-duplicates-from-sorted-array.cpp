5b4ccfe493fe094509440ff138e8747fbeca5520
sha770 <99661850+sha770@users.noreply.github.com>
2025-02-11 20:58:13 +0530

    Time: 3 ms (16.12%), Space: 23.5 MB (5.3%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set<int>s;
+        for(int i=0;i<nums.size();i++){
+            s.insert(nums[i]);
+        }
+        int i=0;
+        for(auto it:s){
+            nums[i]=it;
+            i++;
+        }
+        int size=s.size();
+        return s.size();
+    }
+};
