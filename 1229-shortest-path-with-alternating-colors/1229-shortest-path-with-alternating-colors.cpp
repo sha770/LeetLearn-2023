d4261d9069379440ad9ed20ec3023cc4171dea01
sha770 <99661850+sha770@users.noreply.github.com>
2025-07-30 08:43:33 +0530

    Time: 7 ms (34.72%), Space: 19.3 MB (44.48%) - LeetHub

---
+++ 1229-shortest-path-with-alternating-colors/1229-shortest-path-with-alternating-colors.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
+        vector<vector<pair<int,int>>>adj(n);
+        for(auto &it: redEdges){
+            adj[it[0]].push_back({it[1],0});
+        }
+        for(auto &it: blueEdges){
+            adj[it[0]].push_back({it[1],1});
+        }
+        vector<int>ans(n,-1);
+        vector<vector<bool>>vis(n,vector<bool>(2,false));
+        queue<vector<int>>q;
+        q.push({0,0,-1});
+        vis[0][0]=true;
+        vis[0][1]=true;
+        ans[0]=0;
+        while(!q.empty()){
+            vector<int>curr=q.front();
+            q.pop();
+            int node=curr[0],step=curr[1],prevclr=curr[2];
+            for(auto &[neighbour,color]:adj[node]){
+                if(!vis[neighbour][color] && color!=prevclr){
+                    vis[neighbour][color]=true;
+                    q.push({neighbour,1+step,color});
+                    if(ans[neighbour]==-1) ans[neighbour]=1+step;
+                }
+            }
+        }
+        return ans;
+    }
+};
