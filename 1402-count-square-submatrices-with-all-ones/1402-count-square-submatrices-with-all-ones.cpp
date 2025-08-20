05653047069e22e25d4fbc5d9c60b47b6df61b17
sha770 <99661850+sha770@users.noreply.github.com>
2025-08-20 10:37:56 +0530

    Time: 0 ms (100%), Space: 27.2 MB (99.9%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int countSquares(vector<vector<int>>& matrix) {
-        int count = 0, m= matrix.size(), n=matrix[0].size();
+      /*  int count = 0, m= matrix.size(), n=matrix[0].size();
          // Recurssion -> check every col as the bootom right of square and check for 1s at one above , one left and one digonal left to frame a square.
         // Start from bottom-right and build up square sizes
         for(int i = 0; i < m; i++) {
@@ -13,6 +13,27 @@ public:
             }
         }
 
-        return count;
+        return count;*/
+
+
+        //DP
+
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int res=0;
+        // first count 1x1 square in first col and first row to add on
+
+        for (int i=0;i<n;i++) res+=matrix[i][0]; //col
+        for (int j=1;j<m;j++) res+=matrix[0][j];
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]==1){
+                    matrix[i][j]=1+min(matrix[i-1][j-1],min(matrix[i][j-1],matrix[i-1][j]));
+                    res+=matrix[i][j];
+                }
+            }
+        }
+        return res;
     }
 };
