e2c1c460b2df768ba0bbac4b6e4279786f842169
sha770 <99661850+sha770@users.noreply.github.com>
2026-02-08 14:13:48 +0530

    Time: 0 ms (100%), Space: 12.4 MB (40.21%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=0,j=0;
+                int i=0,j=0;
         int k=0;
         while(i<m && j<n){
           if(nums1[i]<=nums2[j]){
@@ -23,5 +23,6 @@ public:
             nums1[k++]=nums2[j++];
         }
         
+
     }
 };
